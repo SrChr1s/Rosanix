dbec12b664114e4367d5b4b42737761c086fb9bd
sofiatorres25 <sofiatorres5082@gmail.com>
2024-11-04 03:18:36 -0300

    Landing modified

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,8 @@
 import { Link } from "wouter";
 import Button from "../components/Button";
 import { Layout } from "antd";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 const { Header, Content, Footer } = Layout;
 
 export default function Landing() {
@@ -11,17 +13,23 @@ export default function Landing() {
           <img src="/main-logo.png" alt="logo" />
         </div>
         <div>
-          <Link to="/login">
-            <Button text={"Iniciar Sesión"} />
+        <Link to="/login" className="sm:hidden">
+            <FontAwesomeIcon icon={faUser} className="text-white w-6 h-6 hover:text-gray-300" />
+          </Link>
+          <Link to="/login" className="hidden sm:block">
+            <Button
+              text={"Iniciar Sesión"}
+              className="bg-white text-[#B8C0FF] hover:bg-gray-100 font-semibold py-2 px-4 rounded-md"
+            />
           </Link>
         </div>
       </Header>
       <Content className="flex justify-center py-10 bg-[#BBD0FF]">
-        <div className="flex flex-col w-6/12 py-10 px-12 justify-center items-center rounded-3xl text-center bg-white/25">
-          <h1 className="text-6xl font-[Bebas] mb-10">
+        <div className="flex flex-col w-11/12 sm:w-8/12 lg:w-6/12 py-10 px-12 justify-center items-center rounded-3xl text-center bg-white/25">
+          <h1 className="text-xl sm:text-4xl md:text-5xl lg:text-6xl font-[Bebas] mt-10 mb-4 sm:mt-16 sm:mb-10">
             Toma el control de tu día, tarea a tarea
           </h1>
-          <h2 className="text-lg">
+          <h2 className="text-sm sm:text-lg mb-10 sm:mb-16">
             Rosanix es tu compañero en la gestión de tareas. Organiza, prioriza
             y completa tus pendientes de forma sencilla y segura. Con nuestra
             plataforma, tu productividad está siempre al alcance.
