0e4ff6f9fec1f6844e7be932e95d1348dfbaddd5
sofiatorres25 <sofiatorres5082@gmail.com>
2024-11-05 00:43:09 -0300

    Login modified

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ form = false, text }) {
     <button
       className={`${
         !form ? "px-5 py-2" : "px-20 py-2"
-      } rounded-full bg-[#e299b6] text-white text-lg border-2 border-white hover:bg-[#E7C6FF] duration-200 active:scale-90`}
+      } rounded-full bg-[#e299b6] font-[Nunito] text-white text-lg border-2 border-white hover:bg-[#E7C6FF] duration-200 active:scale-90`}
     >
       {text}
     </button>
