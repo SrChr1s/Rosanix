aed4632f6202a3fe4878727ad43eb2dcf644804e
Chris <c.salazar.vzla@gmail.com>
2024-11-06 04:36:52 -0300

    Home content added and logout endpoint also added

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,3 +5,5 @@ const API = "http://192.168.5.114:4000/api";
 export const loginRequest = (user) => axios.post(`${API}/login`, user);
 
 export const registerRequest = (user) => axios.post(`${API}/register`, user);
+
+export const logoutRequest = () => axios.post(`${API}/logout`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,84 @@
-import React from "react";
+import {
+  FaIdCard,
+  FaIndent,
+  FaKey,
+  FaOutdent,
+  FaPlus,
+  FaUser,
+} from "react-icons/fa6";
+import { useState } from "react";
+import { logoutRequest } from "../api/auth";
+import { useLocation } from "wouter";
+import { Layout, Menu, Button } from "antd";
+const { Header, Content, Sider } = Layout;
 
 export default function Home() {
-  return <div>Home</div>;
+  const [isOpen, setIsOpen] = useState(true);
+  const [location, setLocation] = useLocation();
+
+  const handleMenuClick = async (e) => {
+    if (e.key == 3) {
+      await logoutRequest();
+      setLocation("/");
+    }
+  };
+
+  return (
+    <Layout className="overflow-hidden">
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={isOpen}
+        className="h-dvh *:flex *:flex-col"
+      >
+        <img
+          src="/logo-rosanix.png"
+          alt="logo"
+          className="w-16 mt-2 mb-10 self-center"
+        />
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectable={false}
+          onClick={handleMenuClick}
+          items={[
+            {
+              key: 1,
+              icon: <FaPlus />,
+              label: "Nueva Tarea",
+            },
+            {
+              key: "sub1",
+              icon: <FaUser />,
+              label: "Mi Perfil",
+              type: "submenu",
+              children: [
+                {
+                  key: 2,
+                  icon: <FaIdCard />,
+                  label: "Mis Datos",
+                },
+                {
+                  key: 3,
+                  icon: <FaKey />,
+                  label: "Cerrar Sesión",
+                },
+              ],
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header className="flex items-center w-dvw p-0">
+          <Button
+            type="text"
+            icon={isOpen ? <FaIndent /> : <FaOutdent />}
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-2xl text-white"
+          />
+        </Header>
+        <Content></Content>
+      </Layout>
+    </Layout>
+  );
 }
