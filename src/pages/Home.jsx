18d0a9d43a75ae0292e944db15a837469a6a4945
Chris <c.salazar.vzla@gmail.com>
2024-11-08 00:45:54 -0300

    home sidebar created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,8 @@
-import {
-  FaIdCard,
-  FaIndent,
-  FaKey,
-  FaOutdent,
-  FaPlus,
-  FaUser,
-} from "react-icons/fa6";
+import { FaIdCard, FaKey, FaPlus } from "react-icons/fa6";
 import { useState } from "react";
 import { logoutRequest } from "../api/auth";
 import { useLocation } from "wouter";
-import { Layout, Menu, Button } from "antd";
+import { ConfigProvider, Layout, Menu } from "antd";
 const { Header, Content, Sider } = Layout;
 
 export default function Home() {
@@ -24,61 +17,81 @@ export default function Home() {
   };
 
   return (
-    <Layout className="overflow-hidden">
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={isOpen}
-        className="h-dvh *:flex *:flex-col"
-      >
-        <img
-          src="/logo-rosanix.png"
-          alt="logo"
-          className="w-16 mt-2 mb-10 self-center"
-        />
-        <Menu
-          theme="dark"
-          mode="inline"
-          selectable={false}
-          onClick={handleMenuClick}
-          items={[
-            {
-              key: 1,
-              icon: <FaPlus />,
-              label: "Nueva Tarea",
-            },
-            {
-              key: "sub1",
-              icon: <FaUser />,
-              label: "Mi Perfil",
-              type: "submenu",
-              children: [
-                {
-                  key: 2,
-                  icon: <FaIdCard />,
-                  label: "Mis Datos",
+    <ConfigProvider
+      theme={{
+        components: {
+          Layout: {
+            triggerBg: "#c3c1f460",
+          },
+        },
+      }}
+    >
+      <Layout className="overflow-hidden">
+        <Sider
+          collapsible
+          collapsed={isOpen}
+          onCollapse={() => setIsOpen(!isOpen)}
+          className="h-dvh bg-[#a4caf5]"
+          width={230}
+        >
+          <div
+            className="logo w-14 mt-3 mb-10 drop-shadow-sm self-center"
+            hidden={!isOpen}
+          >
+            <img src="/logo-rosanix.png" alt="logo" />
+          </div>
+
+          <div
+            className="logo w-48 mt-2 mb-10 drop-shadow-sm self-center"
+            hidden={isOpen}
+          >
+            <img src="/main-logo.png" alt="logo" />
+          </div>
+          <Menu
+            className={`bg-[#c3c1f460] ${
+              !isOpen ? "rounded-3xl w-10/12 self-center" : ""
+            } max-sm:text-xl justify-items-center px-2`}
+            mode="inline"
+            selectable={false}
+            onClick={handleMenuClick}
+            style={{ borderStyle: "none" }}
+            items={[
+              {
+                key: 1,
+                icon: <FaPlus />,
+                label: "Nueva Tarea",
+                style: {
+                  color: "white",
+                  margin: "24px 0",
                 },
-                {
-                  key: 3,
-                  icon: <FaKey />,
-                  label: "Cerrar Sesión",
+              },
+
+              {
+                key: 2,
+                icon: <FaIdCard />,
+                label: "Mis Datos",
+                style: {
+                  color: "white",
+                  margin: "24px 0",
                 },
-              ],
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header className="flex items-center w-dvw p-0">
-          <Button
-            type="text"
-            icon={isOpen ? <FaIndent /> : <FaOutdent />}
-            onClick={() => setIsOpen(!isOpen)}
-            className="text-2xl text-white"
+              },
+              {
+                key: 3,
+                icon: <FaKey />,
+                label: "Cerrar Sesión",
+                style: {
+                  color: "white",
+                  margin: "24px 0",
+                },
+              },
+            ]}
           />
+        </Sider>
+        <Header className="flex items-center w-dvw pl-5 text-white bg-[#a4caf5]">
+          <h1 className="text-3xl">Welcome, user!</h1>
         </Header>
         <Content></Content>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 }
