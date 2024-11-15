2c804daf79d30f93b6fd0f2b9b2c910282a67deb
Chris <c.salazar.vzla@gmail.com>
2024-11-15 06:37:55 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "rosanixapi-production.up.railway.app:8080/api",
+  baseURL: "https://rosanixapi-production.up.railway.app/api",
   withCredentials: true,
 });
 
